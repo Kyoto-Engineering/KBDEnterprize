bc6a4aec8292291a4a0398e4ad4ee01038125294
RahatRahman <mahmudur665@gmail.com>
2017-12-03 16:31:19 +0600

    frmRegistration.Designer.cs

--- PhonebookApp/LogInUI/frmRegistration.Designer.cs
+++ PhonebookApp/LogInUI/frmRegistration.Designer.cs
@@ -52,11 +52,11 @@
             // 
             // emailTextBox
             // 
-            this.emailTextBox.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.emailTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F, System.Drawing.FontStyle.Bold);
             this.emailTextBox.Location = new System.Drawing.Point(187, 186);
             this.emailTextBox.MaxLength = 50;
             this.emailTextBox.Name = "emailTextBox";
-            this.emailTextBox.Size = new System.Drawing.Size(228, 29);
+            this.emailTextBox.Size = new System.Drawing.Size(228, 27);
             this.emailTextBox.TabIndex = 5;
             this.emailTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.emailTextBox_KeyDown);
             this.emailTextBox.Validating += new System.ComponentModel.CancelEventHandler(this.emailTextBox_Validating);
@@ -64,20 +64,20 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F, System.Drawing.FontStyle.Bold);
             this.label8.Location = new System.Drawing.Point(9, 188);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(183, 22);
+            this.label8.Size = new System.Drawing.Size(180, 22);
             this.label8.TabIndex = 16;
-            this.label8.Text = "Email Address          :";
+            this.label8.Text = "Email Address      :";
             // 
             // contactNoTextBox
             // 
-            this.contactNoTextBox.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.contactNoTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F, System.Drawing.FontStyle.Bold);
             this.contactNoTextBox.Location = new System.Drawing.Point(187, 308);
             this.contactNoTextBox.MaxLength = 11;
             this.contactNoTextBox.Name = "contactNoTextBox";
-            this.contactNoTextBox.Size = new System.Drawing.Size(228, 29);
+            this.contactNoTextBox.Size = new System.Drawing.Size(228, 27);
             this.contactNoTextBox.TabIndex = 8;
             this.contactNoTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.contactNoTextBox_KeyDown);
             this.contactNoTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.contactNoTextBox_KeyPress);
@@ -85,101 +85,101 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F, System.Drawing.FontStyle.Bold);
             this.label7.Location = new System.Drawing.Point(9, 310);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(184, 22);
+            this.label7.Size = new System.Drawing.Size(182, 22);
             this.label7.TabIndex = 14;
-            this.label7.Text = "Contact No               :";
+            this.label7.Text = "Contact No           :";
             // 
             // departmentTextBox
             // 
-            this.departmentTextBox.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.departmentTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F, System.Drawing.FontStyle.Bold);
             this.departmentTextBox.Location = new System.Drawing.Point(187, 263);
             this.departmentTextBox.MaxLength = 50;
             this.departmentTextBox.Name = "departmentTextBox";
-            this.departmentTextBox.Size = new System.Drawing.Size(228, 29);
+            this.departmentTextBox.Size = new System.Drawing.Size(228, 27);
             this.departmentTextBox.TabIndex = 7;
             this.departmentTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.departmentTextBox_KeyDown);
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F, System.Drawing.FontStyle.Bold);
             this.label6.Location = new System.Drawing.Point(9, 265);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(182, 22);
+            this.label6.Size = new System.Drawing.Size(179, 22);
             this.label6.TabIndex = 12;
-            this.label6.Text = "Department              :";
+            this.label6.Text = "Department          :";
             // 
             // designationTextBox
             // 
-            this.designationTextBox.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.designationTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F, System.Drawing.FontStyle.Bold);
             this.designationTextBox.Location = new System.Drawing.Point(187, 224);
             this.designationTextBox.MaxLength = 50;
             this.designationTextBox.Name = "designationTextBox";
-            this.designationTextBox.Size = new System.Drawing.Size(228, 29);
+            this.designationTextBox.Size = new System.Drawing.Size(228, 27);
             this.designationTextBox.TabIndex = 6;
             this.designationTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.designationTextBox_KeyDown);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F, System.Drawing.FontStyle.Bold);
             this.label5.Location = new System.Drawing.Point(9, 226);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(182, 22);
             this.label5.TabIndex = 10;
-            this.label5.Text = "Designation              :";
+            this.label5.Text = "Designation          :";
             // 
             // userNameTextBox
             // 
-            this.userNameTextBox.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userNameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F, System.Drawing.FontStyle.Bold);
             this.userNameTextBox.Location = new System.Drawing.Point(187, 27);
             this.userNameTextBox.MaxLength = 50;
             this.userNameTextBox.Name = "userNameTextBox";
-            this.userNameTextBox.Size = new System.Drawing.Size(228, 29);
+            this.userNameTextBox.Size = new System.Drawing.Size(228, 27);
             this.userNameTextBox.TabIndex = 1;
             this.userNameTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.userNameTextBox_KeyDown);
             // 
             // passwordTextBox
             // 
-            this.passwordTextBox.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F, System.Drawing.FontStyle.Bold);
             this.passwordTextBox.Location = new System.Drawing.Point(187, 66);
             this.passwordTextBox.MaxLength = 50;
             this.passwordTextBox.Name = "passwordTextBox";
             this.passwordTextBox.PasswordChar = '*';
-            this.passwordTextBox.Size = new System.Drawing.Size(228, 29);
+            this.passwordTextBox.Size = new System.Drawing.Size(228, 27);
             this.passwordTextBox.TabIndex = 2;
             this.passwordTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.passwordTextBox_KeyDown);
             // 
             // txtUserComboBox
             // 
-            this.txtUserComboBox.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUserComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F, System.Drawing.FontStyle.Bold);
             this.txtUserComboBox.FormattingEnabled = true;
             this.txtUserComboBox.Items.AddRange(new object[] {
             "Admin",
             "User"});
             this.txtUserComboBox.Location = new System.Drawing.Point(187, 104);
             this.txtUserComboBox.Name = "txtUserComboBox";
-            this.txtUserComboBox.Size = new System.Drawing.Size(228, 29);
+            this.txtUserComboBox.Size = new System.Drawing.Size(228, 30);
             this.txtUserComboBox.TabIndex = 3;
             this.txtUserComboBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtUserComboBox_KeyDown);
             // 
             // nameTextBox
             // 
-            this.nameTextBox.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F, System.Drawing.FontStyle.Bold);
             this.nameTextBox.Location = new System.Drawing.Point(187, 145);
             this.nameTextBox.MaxLength = 50;
             this.nameTextBox.Name = "nameTextBox";
-            this.nameTextBox.Size = new System.Drawing.Size(228, 29);
+            this.nameTextBox.Size = new System.Drawing.Size(228, 27);
             this.nameTextBox.TabIndex = 4;
             this.nameTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.nameTextBox_KeyDown);
             // 
             // userButton
             // 
             this.userButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
-            this.userButton.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
             this.userButton.ForeColor = System.Drawing.Color.Blue;
             this.userButton.Location = new System.Drawing.Point(291, 363);
             this.userButton.Name = "userButton";
@@ -192,42 +192,42 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F, System.Drawing.FontStyle.Bold);
             this.label4.Location = new System.Drawing.Point(9, 147);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(185, 22);
+            this.label4.Size = new System.Drawing.Size(178, 22);
             this.label4.TabIndex = 3;
-            this.label4.Text = "Full Name                 :";
+            this.label4.Text = "Full Name            :";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F, System.Drawing.FontStyle.Bold);
             this.label3.Location = new System.Drawing.Point(9, 106);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(183, 22);
+            this.label3.Size = new System.Drawing.Size(181, 22);
             this.label3.TabIndex = 2;
-            this.label3.Text = "User Type                 :";
+            this.label3.Text = "User Type            :";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F, System.Drawing.FontStyle.Bold);
             this.label2.Location = new System.Drawing.Point(9, 69);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(185, 22);
+            this.label2.Size = new System.Drawing.Size(181, 22);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Password                  :";
+            this.label2.Text = "Password             :";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F, System.Drawing.FontStyle.Bold);
             this.label1.Location = new System.Drawing.Point(9, 30);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(181, 22);
             this.label1.TabIndex = 0;
-            this.label1.Text = "User Name               :";
+            this.label1.Text = "User Name           :";
             // 
             // groupBox1
             // 

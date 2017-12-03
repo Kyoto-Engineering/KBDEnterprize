e57cfce48c91e3337e53f6e7886af303447eafc8
RahatRahman <mahmudur665@gmail.com>
2017-12-03 16:31:18 +0600

    UserManagementUI.Designer.cs

--- PhonebookApp/LogInUI/UserManagementUI.Designer.cs
+++ PhonebookApp/LogInUI/UserManagementUI.Designer.cs
@@ -39,11 +39,11 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold);
             this.label1.ForeColor = System.Drawing.Color.Blue;
             this.label1.Location = new System.Drawing.Point(345, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(264, 31);
+            this.label1.Size = new System.Drawing.Size(289, 31);
             this.label1.TabIndex = 0;
             this.label1.Text = "User Management UI";
             // 
@@ -61,8 +61,9 @@
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
             this.button1.ForeColor = System.Drawing.Color.Blue;
-            this.button1.Location = new System.Drawing.Point(15, 118);
+            this.button1.Location = new System.Drawing.Point(13, 118);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(123, 61);
             this.button1.TabIndex = 4;
@@ -73,8 +74,9 @@
             // button3
             // 
             this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
             this.button3.ForeColor = System.Drawing.Color.Blue;
-            this.button3.Location = new System.Drawing.Point(15, 25);
+            this.button3.Location = new System.Drawing.Point(13, 25);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(123, 60);
             this.button3.TabIndex = 3;

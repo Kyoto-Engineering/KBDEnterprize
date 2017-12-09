99e9860bc5a98a63809f8cd41af54d23f5a169d0
Saqib-Tanmoy <saqib.tanmoy@gmail.com>
2017-12-09 15:51:14 +0600

    ui modified

--- PhonebookApp/Reports/LOIforAutomatedTrafficLightUI.Designer.cs
+++ PhonebookApp/Reports/LOIforAutomatedTrafficLightUI.Designer.cs
@@ -38,9 +38,10 @@
             // resedentialRadioButton
             // 
             this.resedentialRadioButton.AutoSize = true;
-            this.resedentialRadioButton.Location = new System.Drawing.Point(177, 125);
+            this.resedentialRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
+            this.resedentialRadioButton.Location = new System.Drawing.Point(147, 142);
             this.resedentialRadioButton.Name = "resedentialRadioButton";
-            this.resedentialRadioButton.Size = new System.Drawing.Size(118, 17);
+            this.resedentialRadioButton.Size = new System.Drawing.Size(214, 28);
             this.resedentialRadioButton.TabIndex = 21;
             this.resedentialRadioButton.TabStop = true;
             this.resedentialRadioButton.Text = "Residential Address";
@@ -49,9 +50,10 @@
             // workingRadioButton
             // 
             this.workingRadioButton.AutoSize = true;
-            this.workingRadioButton.Location = new System.Drawing.Point(177, 99);
+            this.workingRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
+            this.workingRadioButton.Location = new System.Drawing.Point(147, 108);
             this.workingRadioButton.Name = "workingRadioButton";
-            this.workingRadioButton.Size = new System.Drawing.Size(106, 17);
+            this.workingRadioButton.Size = new System.Drawing.Size(188, 28);
             this.workingRadioButton.TabIndex = 20;
             this.workingRadioButton.TabStop = true;
             this.workingRadioButton.Text = "Working Address";
@@ -60,9 +62,10 @@
             // getButton
             // 
             this.getButton.BackgroundImage = global::PhonebookApp.Properties.Resources.whiteyglossyrectanglebuttonmd;
-            this.getButton.Location = new System.Drawing.Point(178, 196);
+            this.getButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
+            this.getButton.Location = new System.Drawing.Point(180, 190);
             this.getButton.Name = "getButton";
-            this.getButton.Size = new System.Drawing.Size(75, 23);
+            this.getButton.Size = new System.Drawing.Size(120, 46);
             this.getButton.TabIndex = 19;
             this.getButton.Text = "GET";
             this.getButton.UseVisualStyleBackColor = true;
@@ -70,25 +73,27 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
             this.label1.Location = new System.Drawing.Point(45, 61);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.Size = new System.Drawing.Size(68, 24);
             this.label1.TabIndex = 18;
-            this.label1.Text = "Group Name";
+            this.label1.Text = "Group";
             // 
             // groupComboBox
             // 
+            this.groupComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
             this.groupComboBox.FormattingEnabled = true;
             this.groupComboBox.Location = new System.Drawing.Point(118, 58);
             this.groupComboBox.Name = "groupComboBox";
-            this.groupComboBox.Size = new System.Drawing.Size(292, 21);
+            this.groupComboBox.Size = new System.Drawing.Size(292, 32);
             this.groupComboBox.TabIndex = 17;
             // 
             // LOIforAutomatedTrafficLightUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackColor = System.Drawing.Color.LightBlue;
             this.ClientSize = new System.Drawing.Size(455, 276);
             this.Controls.Add(this.resedentialRadioButton);
             this.Controls.Add(this.workingRadioButton);

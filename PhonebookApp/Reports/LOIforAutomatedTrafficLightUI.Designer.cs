cf0e9ea7faaf0f3193f0037bfa734acfaa43c438
Saqib-Tanmoy <saqib.tanmoy@gmail.com>
2017-12-09 14:49:18 +0600

    ui modified

--- PhonebookApp/Reports/LOIforAutomatedTrafficLightUI.Designer.cs
+++ PhonebookApp/Reports/LOIforAutomatedTrafficLightUI.Designer.cs
@@ -59,6 +59,7 @@
             // 
             // getButton
             // 
+            this.getButton.BackgroundImage = global::PhonebookApp.Properties.Resources.whiteyglossyrectanglebuttonmd;
             this.getButton.Location = new System.Drawing.Point(178, 196);
             this.getButton.Name = "getButton";
             this.getButton.Size = new System.Drawing.Size(75, 23);

84d1d7ab4dca5592695432b0c90587f1abbf5af2
Saqib-Tanmoy <saqib.tanmoy@gmail.com>
2017-12-11 15:52:02 +0600

    UI Modified

--- PhonebookApp/UI/GroupCreation.Designer.cs
+++ PhonebookApp/UI/GroupCreation.Designer.cs
@@ -42,32 +42,31 @@
             // groupNamelabel
             // 
             this.groupNamelabel.AutoSize = true;
-            this.groupNamelabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
-            this.groupNamelabel.Location = new System.Drawing.Point(163, 114);
+            this.groupNamelabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupNamelabel.Location = new System.Drawing.Point(100, 114);
             this.groupNamelabel.Name = "groupNamelabel";
-            this.groupNamelabel.Size = new System.Drawing.Size(129, 24);
+            this.groupNamelabel.Size = new System.Drawing.Size(110, 20);
             this.groupNamelabel.TabIndex = 7;
             this.groupNamelabel.Text = "Group Name";
             // 
             // groupNametextBox
             // 
-            this.groupNametextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
-            this.groupNametextBox.Location = new System.Drawing.Point(302, 114);
+            this.groupNametextBox.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupNametextBox.Location = new System.Drawing.Point(250, 114);
             this.groupNametextBox.MaxLength = 100;
             this.groupNametextBox.Name = "groupNametextBox";
-            this.groupNametextBox.Size = new System.Drawing.Size(311, 29);
+            this.groupNametextBox.Size = new System.Drawing.Size(425, 29);
             this.groupNametextBox.TabIndex = 1;
             this.groupNametextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.groupNametextBox_KeyDown);
             // 
             // savebutton
             // 
-            this.savebutton.BackColor = System.Drawing.Color.CadetBlue;
-            this.savebutton.BackgroundImage = global::PhonebookApp.Properties.Resources.whiteyglossyrectanglebuttonmd;
-            this.savebutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold);
-            this.savebutton.ForeColor = System.Drawing.Color.Blue;
-            this.savebutton.Location = new System.Drawing.Point(409, 360);
+            this.savebutton.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.savebutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.savebutton.ForeColor = System.Drawing.Color.Black;
+            this.savebutton.Location = new System.Drawing.Point(400, 360);
             this.savebutton.Name = "savebutton";
-            this.savebutton.Size = new System.Drawing.Size(104, 66);
+            this.savebutton.Size = new System.Drawing.Size(113, 38);
             this.savebutton.TabIndex = 4;
             this.savebutton.Text = "Save";
             this.savebutton.UseVisualStyleBackColor = false;
@@ -76,29 +75,29 @@
             // definitionlabel
             // 
             this.definitionlabel.AutoSize = true;
-            this.definitionlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
-            this.definitionlabel.Location = new System.Drawing.Point(189, 161);
+            this.definitionlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.definitionlabel.Location = new System.Drawing.Point(123, 161);
             this.definitionlabel.Name = "definitionlabel";
-            this.definitionlabel.Size = new System.Drawing.Size(97, 24);
+            this.definitionlabel.Size = new System.Drawing.Size(86, 20);
             this.definitionlabel.TabIndex = 10;
             this.definitionlabel.Text = "Definition";
             // 
             // purposelabel
             // 
             this.purposelabel.AutoSize = true;
-            this.purposelabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
-            this.purposelabel.Location = new System.Drawing.Point(202, 303);
+            this.purposelabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.purposelabel.Location = new System.Drawing.Point(136, 303);
             this.purposelabel.Name = "purposelabel";
-            this.purposelabel.Size = new System.Drawing.Size(88, 24);
+            this.purposelabel.Size = new System.Drawing.Size(75, 20);
             this.purposelabel.TabIndex = 12;
             this.purposelabel.Text = "Purpose";
             // 
             // definitionrichTextBox
             // 
-            this.definitionrichTextBox.Location = new System.Drawing.Point(302, 161);
+            this.definitionrichTextBox.Location = new System.Drawing.Point(250, 161);
             this.definitionrichTextBox.MaxLength = 500;
             this.definitionrichTextBox.Name = "definitionrichTextBox";
-            this.definitionrichTextBox.Size = new System.Drawing.Size(311, 121);
+            this.definitionrichTextBox.Size = new System.Drawing.Size(425, 121);
             this.definitionrichTextBox.TabIndex = 2;
             this.definitionrichTextBox.Text = "";
             this.definitionrichTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.definitionrichTextBox_KeyDown);
@@ -106,21 +105,21 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Black;
             this.label3.Location = new System.Drawing.Point(341, 24);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(228, 33);
+            this.label3.Size = new System.Drawing.Size(213, 31);
             this.label3.TabIndex = 15;
             this.label3.Text = "Group Creation";
             // 
             // purposetextBox
             // 
-            this.purposetextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
-            this.purposetextBox.Location = new System.Drawing.Point(302, 303);
+            this.purposetextBox.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.purposetextBox.Location = new System.Drawing.Point(250, 303);
             this.purposetextBox.MaxLength = 200;
             this.purposetextBox.Name = "purposetextBox";
-            this.purposetextBox.Size = new System.Drawing.Size(311, 29);
+            this.purposetextBox.Size = new System.Drawing.Size(425, 29);
             this.purposetextBox.TabIndex = 3;
             this.purposetextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.purposetextBox_KeyDown);
             // 
@@ -128,8 +127,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.LightBlue;
-            this.ClientSize = new System.Drawing.Size(822, 491);
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(822, 452);
             this.Controls.Add(this.purposetextBox);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.definitionrichTextBox);

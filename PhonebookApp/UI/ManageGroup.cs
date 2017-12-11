6cddd44bc73188d1369be514391b3dd835d5761f
Saqib-Tanmoy <saqib.tanmoy@gmail.com>
2017-12-11 15:39:38 +0600

    UI Modified

---
+++ PhonebookApp/UI/ManageGroup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhonebookApp.UI
+{
+    public partial class ManageGroup : Form
+    {
+        public ManageGroup()
+        {
+            InitializeComponent();
+        }
+
+        private void ManageGroupsbutton_Click(object sender, EventArgs e)
+        {
+            RemoveFromGroup rfg = new RemoveFromGroup();
+            this.Visible = false;
+            rfg.ShowDialog();
+            this.Visible = true; 
+        }
+
+        private void MemberAddedToGroupbutton_Click(object sender, EventArgs e)
+        {
+            //this.Hide();
+            Group frm = new Group();
+            this.Visible = false;
+            frm.ShowDialog();
+            this.Visible = true;
+        }
+
+        private void NewGroupCreationbutton_Click(object sender, EventArgs e)
+        {
+            GroupCreation frm = new GroupCreation();
+            this.Visible = false;
+            frm.ShowDialog();
+            this.Visible = true;
+        }
+    }
+}

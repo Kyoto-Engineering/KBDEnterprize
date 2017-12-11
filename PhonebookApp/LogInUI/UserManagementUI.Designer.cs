7f37145a32ac196b9180c0854d6a28d09366fcc9
Saqib-Tanmoy <saqib.tanmoy@gmail.com>
2017-12-11 13:14:36 +0600

    ui modified

--- PhonebookApp/LogInUI/UserManagementUI.Designer.cs
+++ PhonebookApp/LogInUI/UserManagementUI.Designer.cs
@@ -63,7 +63,7 @@
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
             this.button1.BackgroundImage = global::PhonebookApp.Properties.Resources.whiteyglossyrectanglebuttonmd;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
-            this.button1.ForeColor = System.Drawing.Color.Blue;
+            this.button1.ForeColor = System.Drawing.Color.Navy;
             this.button1.Location = new System.Drawing.Point(13, 118);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(123, 61);
@@ -77,7 +77,7 @@
             this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
             this.button3.BackgroundImage = global::PhonebookApp.Properties.Resources.whiteyglossyrectanglebuttonmd;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
-            this.button3.ForeColor = System.Drawing.Color.Blue;
+            this.button3.ForeColor = System.Drawing.Color.Navy;
             this.button3.Location = new System.Drawing.Point(13, 25);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(123, 60);
